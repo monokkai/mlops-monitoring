9771068d99e9c88072c608a93dd1348674e6fbc4
monokkai <egormayer08@mail.ru>
2025-12-07 20:04:23 +0200

    upd: updated main.ts file

--- src/api-gateway/src/main.ts
+++ src/api-gateway/src/main.ts
@@ -1,11 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  app.enableCors();
+  const logger = new Logger("Bootstrap");
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN || '*',
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }
+  );
   app.setGlobalPrefix('api');
+
 
   await app.listen(process.env.PORT ?? 80);
   console.log(`🚀 API Gateway is running at http://localhost:${process.env.PORT}`);
