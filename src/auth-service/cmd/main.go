c08d1f62c6693d82f5f56c5890288b37a2213e7b
monokkai <egormayer08@mail.ru>
2025-12-09 22:35:44 +0200

    dev: pong func to check service

--- src/auth-service/cmd/main.go
+++ src/auth-service/cmd/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-	"net/http"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	r := gin.Default()
 
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+
+	r.GET("/ping", Pong)
 }
---
+++ src/auth-service/cmd/routes.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func Pong(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "Pong!"})
+}
