b23a5c8b489b9c9e18413d2f931ed3cc39ab2505
monokkai <egormayer08@mail.ru>
2025-12-11 00:09:32 +0200

    dev: api-gateway works

--- src/api-gateway/src/app.module.ts
+++ src/api-gateway/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from "@nestjs/config";
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { ConfigModule } from "@nestjs/config";
       isGlobal: true,
       envFilePath: '../.env'
     }),
+    HttpModule
   ],
   controllers: [AppController],
   providers: [AppService],
